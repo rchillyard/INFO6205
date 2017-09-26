276de46fbef9470f5673961d6a3474e477b3bd16
Scala Prof <scalaprof@gmail.com>
2017-09-26 11:13:57 -0400

    Added additional unit tests

--- src/test/java/edu/neu/coe/info6205/randomwalk/RandomWalkTest.java
+++ src/test/java/edu/neu/coe/info6205/randomwalk/RandomWalkTest.java
@@ -15,7 +15,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class RandomWalkTest {
 
     /**
-     * Test method for Bag
+     */
+    @Test
+    public void testMove1() {
+        RandomWalk rw = new RandomWalk();
+        rw.move(1, 0);
+        assertEquals(rw.distance(), 1.0, 1.0E-7);
+        rw.move(1, 0);
+        assertEquals(rw.distance(), 2.0, 1.0E-7);
+        rw.move(-1, 0);
+        assertEquals(rw.distance(), 1.0, 1.0E-7);
+        rw.move(-1, 0);
+        assertEquals(rw.distance(), 0.0, 1.0E-7);
+    }
+
+    /**
+     */
+    @Test
+    public void testMove2() {
+        RandomWalk rw = new RandomWalk();
+        rw.move(0, 1);
+        assertEquals(rw.distance(), 1.0, 1.0E-7);
+        rw.move(0, 1);
+        assertEquals(rw.distance(), 2.0, 1.0E-7);
+        rw.move(0, -1);
+        assertEquals(rw.distance(), 1.0, 1.0E-7);
+        rw.move(0, -1);
+        assertEquals(rw.distance(), 0.0, 1.0E-7);
+    }
+
+    /**
+     */
+    @Test
+    public void testMove3() {
+        RandomWalk rw = new RandomWalk();
+        double root2 = Math.sqrt(2);
+        rw.move(1, 1);
+        assertEquals(rw.distance(), root2, 1.0E-7);
+        rw.move(1, 1);
+        assertEquals(rw.distance(), 2 * root2, 1.0E-7);
+        rw.move(0, -2);
+        assertEquals(rw.distance(), 2.0, 1.0E-7);
+        rw.move(-2, 0);
+        assertEquals(rw.distance(), 0.0, 1.0E-7);
+    }
+
+    /**
      */
     @Test
     public void testRandomWalk() {
