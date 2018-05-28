7037e56a571749a6570ca47ff41ec15bc1b359a9
Scala Prof <scalaprof@gmail.com>
2018-05-28 16:02:20 -0400

    More refactoring of RandomWalk

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -13,43 +13,53 @@ public class RandomWalk {
     private final Random random = new Random();
 
     private void move(int dx, int dy) {
-        // TODO you need to implement this
+        x += dx;
+        y += dy;
     }
 
-    private void randomWalk(int n) {
-        for (int i = 0; i < n; i++)
+    /**
+     * Perform a random walk of m steps
+     * @param m the number of steps the drunkard takes
+     */
+    private void randomWalk(int m) {
+        for (int i = 0; i < m; i++)
             randomMove();
     }
 
     private void randomMove() {
-        // TODO you need to implement this
+        boolean ns = random.nextBoolean();
+        int step = random.nextBoolean() ? 1 : -1;
+        move(ns ? step : 0, ns ? 0 : step);
     }
 
     public double distance() {
-        return 0; // TODO you need to implement this
+        return Math.sqrt(x * x + y * y); // TODO you need to implement this
     }
 
     /**
      * Perform multiple random walk experiments, returning the mean distance.
-     * @param n the number of experiments to run
      * @param m the number of steps for each experiment
+     * @param n the number of experiments to run
      * @return the mean distance
      */
-    public double randomWalkMulti(int n, int m) {
+    public static double randomWalkMulti(int m, int n) {
         double totalDistance = 0;
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < n; i++){
             RandomWalk walk = new RandomWalk();
-            walk.randomWalk(n);
+            walk.randomWalk(m);
             totalDistance = totalDistance + walk.distance();
         }
-        return totalDistance/m ;
+        return totalDistance/n ;
     }
 
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        RandomWalk walk = new RandomWalk();
-        walk.randomWalk(n);
-        System.out.println(n + " steps: " + walk.distance());
+        if (args.length==0)
+            throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
+        int m = Integer.parseInt(args[0]);
+        int n = 30;
+        if (args.length > 1) n = Integer.parseInt(args[1]);
+        double meanDistance = randomWalkMulti(m, n);
+        System.out.println(m + " steps: " + meanDistance + " over "+ n + " experiments");
     }
 
 }
