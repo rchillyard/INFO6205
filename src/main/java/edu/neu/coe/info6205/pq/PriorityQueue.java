1e29185934b922989d0d6f1449971b5e4a37bbee
Scala Prof <scalaprof@gmail.com>
2018-06-26 00:19:33 -0400

    Finished PriorityQueue

--- src/main/java/edu/neu/coe/info6205/pq/PriorityQueue.java
+++ src/main/java/edu/neu/coe/info6205/pq/PriorityQueue.java
@@ -1,47 +1,77 @@
 package edu.neu.coe.info6205.pq;
 
-import java.util.Arrays;
 import java.util.Comparator;
 
 public class PriorityQueue<K> {
 
-    private final boolean max;
-    private final Comparator<K> comparator;
-    private final K[] binHeap; // binHeap[i] is ith element of binary heap
-    private int last; // number of elements in the binary heap
-
-    public PriorityQueue(boolean max, Object[] binHeap, Comparator<K> comparator) {
+    /**
+     * Basic constructor that takes the max value, an actually array of elements, and a comparator.
+     *
+     * @param max        whether or not this is a Maximum Priority Queue as opposed to a Minimum PQ.
+     * @param binHeap    a pre-formed array.
+     * @param last       the number of elements in binHeap
+     * @param comparator a comparator for the type K
+     */
+    public PriorityQueue(boolean max, Object[] binHeap, int last, Comparator<K> comparator) {
 
         this.max = max;
         this.comparator = comparator;
-        this.last = 0;
-        this.binHeap =  (K[])binHeap;
-
+        this.last = last;
+        //noinspection unchecked
+        this.binHeap = (K[]) binHeap;
     }
 
+    /**
+     * Constructor which takes only the priority queue's maximum capacity and a comparator
+     * @param n the desired maximum capacity.
+     * @param max whether or not this is a Maximum Priority Queue as opposed to a Minimum PQ.
+     * @param comparator a comparator for the type K
+     */
     public PriorityQueue (int n, boolean max, Comparator<K> comparator) {
 
-        this(max, new Object[n], comparator);
+        this(max, new Object[n], 0, comparator);
     }
 
+    /**
+     * Constructor which takes only the priority queue's maximum capacity and a comparator
+     * @param n the desired maximum capacity.
+     * @param comparator a comparator for the type K
+     */
     public PriorityQueue (int n, Comparator<K> comparator) {
 
         this(n, true, comparator);
     }
 
+    /**
+     * @return true if the current size is zero.
+     */
     public boolean isEmpty() {
         return last == 0;
     }
 
+    /**
+     * @return the number of elements actually stored in this Priority Queue
+     */
+    public int size() {
+        return last;
+    }
+
+    /**
+     * Insert an element with the given key into this Priority Queue.
+     *
+     * @param key the value of the key to insert
+     */
     public void insert(K key) {
         binHeap[++last] = key;
         swimUp(last);
     }
 
-    public int size() {
-        return last;
-    }
-
+    /**
+     * Take the root element from this Priority Queue.
+     * If max is true, then the result will be the maximum element, else the minimum element.
+     * NOTE that this method is called DelMax (or DelMin) in the book.
+     * @return If max is true, then the maximum element, otherwise the minimum element.
+     */
     public K take() {
         K result = binHeap[1];
         swap(1, last--);
@@ -50,48 +80,83 @@ public class PriorityQueue<K> {
         return result;
     }
 
-    private void sink(int i) {
-        int k = i;
-        while (firstChild(k) <= last) {
-            int j = firstChild(k);
-            if (j < last && unordered(j, j+1)) j++;
-            if (!unordered(k, j)) break;
-            swap(k, j);
-            k = j;
+    /**
+     * Sink the element at index k down
+     */
+    private void sink(@SuppressWarnings("SameParameterValue") int k) {
+        int i = k;
+        while (firstChild(i) <= last) {
+            int j = firstChild(i);
+            if (j < last && unordered(j, j + 1)) j++;
+            if (!unordered(i, j)) break;
+            swap(i, j);
+            i = j;
         }
     }
 
+    /**
+     * Swim the element at index k up
+     */
     private void swimUp(int k) {
-        while (k > 1 && unordered(parent(k),k)) {
-            swap(k, parent(k));
-            k = parent(k);
+        int i = k;
+        while (i > 1 && unordered(parent(i), i)) {
+            swap(i, parent(i));
+            i = parent(i);
         }
     }
 
+    /**
+     * Exchange the values at indices i and j
+     */
     private void swap(int i, int j) {
         K tmp = binHeap[i];
         binHeap[i] = binHeap[j];
         binHeap[j] = tmp;
     }
 
+    /**
+     * Compare the elements at indices i and j.
+     * We expect the first index (the smaller one) to be greater than the second, assuming that max is true.
+     * In this case, we return false.
+     *
+     * @param i the lower index, numerically
+     * @param j the higher index, numerically
+     * @return true if the values are out of order.
+     */
     private boolean unordered(int i, int j) {
-        return (comparator.compare(binHeap[i],binHeap[j])<0) ^ max;
-    }
-
-    private int parent(int k) {
-        return k/2;
-    }
-
-    private int firstChild(int k) {
-        return k*2;
+        return (comparator.compare(binHeap[i], binHeap[j]) > 0) ^ max;
     }
 
     /**
-     * For testing only.
-     * @param k
-     * @return
+     * Get the index of the parent of the element at index k
      */
+    private int parent(int k) {
+        return k / 2;
+    }
+
+    /**
+     * Get the index of the first child of the element at index k.
+     * The index of the second child will be one greater than the result.
+     */
+    private int firstChild(int k) {
+        return k * 2;
+    }
+
+    /**
+     * The following methods are for unit testing ONLY!!
+     */
+
     private K peek(int k) {
         return binHeap[k];
     }
+
+    private boolean getMax() {
+        return max;
+    }
+
+    private final boolean max;
+    private final Comparator<K> comparator;
+    private final K[] binHeap; // binHeap[i] is ith element of binary heap
+    private int last; // number of elements in the binary heap
+
 }
--- src/test/java/edu/neu/coe/info6205/pq/PriorityQueueTest.java
+++ src/test/java/edu/neu/coe/info6205/pq/PriorityQueueTest.java
@@ -6,8 +6,64 @@ import org.junit.Test;
 import java.util.Comparator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PriorityQueueTest {
+
+    @Test
+    public void testMax() {
+
+        PriorityQueue<String> pq = new PriorityQueue<>(10, false, Comparator.comparing(String::toString));
+        final PrivateMethodTester tester = new PrivateMethodTester(pq);
+        assertEquals(false, tester.invokePrivate("getMax"));
+    }
+
+    @Test
+    public void testUnordered1() {
+        String[] binHeap = new String[3];
+        binHeap[1] = "A";
+        binHeap[2] = "B";
+        boolean max = false;
+        PriorityQueue<String> pq = new PriorityQueue<>(max, binHeap, 2, Comparator.comparing(String::toString));
+        final PrivateMethodTester tester = new PrivateMethodTester(pq);
+        assertEquals(max, tester.invokePrivate("unordered", 1, 2));
+    }
+
+    @Test
+    public void testUnordered2() {
+        String[] binHeap = new String[3];
+        binHeap[1] = "A";
+        binHeap[2] = "B";
+        boolean max = true;
+        PriorityQueue<String> pq = new PriorityQueue<>(max, binHeap, 2, Comparator.comparing(String::toString));
+        final PrivateMethodTester tester = new PrivateMethodTester(pq);
+        assertEquals(max, tester.invokePrivate("unordered", 1, 2));
+    }
+
+    @Test
+    public void testSwimUp() {
+        String[] binHeap = new String[3];
+        binHeap[1] = "A";
+        binHeap[2] = "B";
+        PriorityQueue<String> pq = new PriorityQueue<>(true, binHeap, 2, Comparator.comparing(String::toString));
+        final PrivateMethodTester tester = new PrivateMethodTester(pq);
+        assertEquals("A", tester.invokePrivate("peek", 1));
+        tester.invokePrivate("swimUp", 2);
+        assertEquals("B", tester.invokePrivate("peek", 1));
+    }
+
+    @Test
+    public void testSink() {
+        String[] binHeap = new String[4];
+        binHeap[1] = "A";
+        binHeap[2] = "B";
+        binHeap[3] = "C";
+        PriorityQueue<String> pq = new PriorityQueue<>(true, binHeap, 3, Comparator.comparing(String::toString));
+        final PrivateMethodTester tester = new PrivateMethodTester(pq);
+        tester.invokePrivate("sink", 1);
+        assertEquals("C", tester.invokePrivate("peek", 1));
+        assertEquals("A", tester.invokePrivate("peek", 3));
+    }
 
     @Test
     public void testInsert() {
@@ -18,6 +74,32 @@ public class PriorityQueueTest {
         assertEquals(1,pq.size());
         final PrivateMethodTester tester = new PrivateMethodTester(pq);
         assertEquals(key, tester.invokePrivate("peek", 1));
+
+    }
+    @Test
+    public void testTake1() {
+
+        PriorityQueue<String> pq = new PriorityQueue<>(10, Comparator.comparing(String::toString));
+        String key = "A";
+        pq.insert(key);
+        assertEquals(key,pq.take());
+        assertTrue(pq.isEmpty());
+
+    }
+    @Test
+    public void testTake2() {
+
+        PriorityQueue<String> pq = new PriorityQueue<>(10, Comparator.comparing(String::toString));
+        String key1 = "A";
+        String key2 = "B";
+        pq.insert(key1);
+        pq.insert(key2);
+        final PrivateMethodTester tester = new PrivateMethodTester(pq);
+        assertEquals(key1, tester.invokePrivate("peek", 2));
+        assertEquals(key2, tester.invokePrivate("peek", 1));
+        assertEquals(key2,pq.take());
+        assertEquals(key1,pq.take());
+        assertTrue(pq.isEmpty());
 
     }
 }
