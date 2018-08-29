3771cfabbecea19bf25131a541243855e5c8f45b
Robin Hillyard <scalaprof@gmail.com>
2018-08-29 13:04:07 -0400

    Added Greedy
    Added Greedy class (a generic greedy algorithm);
    Created GreedyTest including a Zeckendorf application;
    Refactored Zeckendorf for slightly better efficiency and clarity.

--- src/main/java/edu/neu/coe/info6205/greedy/Zeckendorf.java
+++ src/main/java/edu/neu/coe/info6205/greedy/Zeckendorf.java
@@ -1,7 +1,14 @@
 package edu.neu.coe.info6205.greedy;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
+/**
+ * This class models Zeckendorf's Theorem: https://en.wikipedia.org/wiki/Zeckendorf%27s_theorem
+ * It is an example of a greedy algorithm.
+ * This class does all its work directly (without using the Greedy class);
+ * for an example of Zeckendorf which does use Greedy, please see GreedyTest.java
+ */
 public class Zeckendorf {
 
     public Zeckendorf() {
@@ -11,27 +18,44 @@ public class Zeckendorf {
     }
 
     /**
-     * Get the Zeckendorf components of x
+     * Get the Zeckendorf representation of x
      *
      * @param x a positive number
      * @return a list of longs, each of which is a non-consecutive Fibonacci number, and which sum to x
      */
     public Iterable<Long> get(long x) {
-        ArrayList<Long> result = new ArrayList<>();
+        ensureFibonacci(x);
+        return getZeckendorfRepresentation(x);
+    }
+
+    // This method gets the Zeckendorf representation for x
+    private Iterable<Long> getZeckendorfRepresentation(long x) {
+        Collection<Long> result = new ArrayList<>();
         long remainder = x;
         while (remainder > 0) {
-            long largest = getLargestFibonacci(remainder);
-            result.add(largest);
-            remainder = remainder - largest;
+            long greedy = getLargestFibonacci(remainder);
+            result.add(greedy);
+            remainder = remainder - greedy;
         }
         return result;
     }
 
+    // This is the definition of the Fibonacci series.
+    // NOTE: caller must ensure that 2 <= i <= fibonacci.length
+    private long calculateFibonacci(int i) {
+        return fibonacci[i - 2] + fibonacci[i - 1];
+    }
+
+    // Get the largest Fibonacci number which is smaller than x
     private long getLargestFibonacci(long x) {
+        int index = fibonacci.length - 1;
+        while (fibonacci[index] > x) index--;
+        return fibonacci[index];
+    }
+
+    // Ensure the set of Fibonacci numbers is long enough
+    private void ensureFibonacci(long x) {
         while (fibonacci[fibonacci.length - 1] < x) extendFibonacci();
-        int index = fibonacci.length;
-        while (fibonacci[index - 1] > x) index--;
-        return fibonacci[index - 1];
     }
 
     private void extendFibonacci() {
@@ -39,8 +63,8 @@ public class Zeckendorf {
         long[] temp = new long[length * 2];
         System.arraycopy(fibonacci, 0, temp, 0, length);
         fibonacci = temp;
-        for (int i = length; i < 2 * length; i++)
-            fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+        for (int i = length; i < temp.length; i++)
+            fibonacci[i] = calculateFibonacci(i);
     }
 
     // for testing only
--- src/test/java/edu/neu/coe/info6205/greedy/GreedyTest.java
+++ src/test/java/edu/neu/coe/info6205/greedy/GreedyTest.java
@@ -3,19 +3,100 @@ package edu.neu.coe.info6205.greedy;
 import edu.neu.coe.info6205.util.PrivateMethodTester;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-public class ZeckendorfTest {
+public class GreedyTest {
+
+    /**
+     * Class to model Zeckendorf's theorem.
+     * See also Zeckendorf.java in the main tree.
+     * This version of Zeckendorf uses the Greedy class.
+     */
+    @SuppressWarnings("Duplicates")
+    class GreedyZ {
+
+        GreedyZ() {
+            fibonacci = new long[2];
+            fibonacci[0] = 1;
+            fibonacci[1] = 1;
+            greedy = new Greedy<>(
+                    this::getLargestFibonacci,
+                    (Long l1, Long l2) -> l1 - l2,
+                    this::concatenate,
+                    (Long l) -> l <= 0
+            );
+        }
+
+        /**
+         * Method to run the greedy algorithm defined by greedy.
+         *
+         * @param x the value for which we want the Zeckendorf representation.
+         * @return the Zeckendorf representation.
+         */
+        ArrayList<Long> run(Long x) {
+            ensureFibonacci(x);
+            return greedy.run(x, new ArrayList<>());
+        }
+
+        // This is the definition of the Fibonacci series.
+        // NOTE: caller must ensure that 2 <= i <= fibonacci.length
+        private long calculateFibonacci(int i) {
+            return fibonacci[i - 2] + fibonacci[i - 1];
+        }
+
+        // We have to do it this cumbersome way because Java is not really a functional language
+        private ArrayList<Long> concatenate(Long l, ArrayList<Long> ls) {
+            ArrayList<Long> r = new ArrayList<>(ls);
+            r.add(l);
+            return r;
+        }
+
+        // Get the largest Fibonacci number which is smaller than x
+        private long getLargestFibonacci(long x) {
+            int index = fibonacci.length - 1;
+            while (fibonacci[index] > x) index--;
+            return fibonacci[index];
+        }
+
+        // Ensure the set of Fibonacci numbers is long enough
+        private void ensureFibonacci(long x) {
+            while (fibonacci[fibonacci.length - 1] < x) extendFibonacci();
+        }
+
+        private void extendFibonacci() {
+            int length = fibonacci.length;
+            long[] temp = new long[length * 2];
+            System.arraycopy(fibonacci, 0, temp, 0, length);
+            fibonacci = temp;
+            for (int i = length; i < temp.length; i++)
+                fibonacci[i] = calculateFibonacci(i);
+        }
+
+        // for testing only
+        private int size() {
+            return fibonacci.length;
+        }
+
+        // for testing only
+        private long fib(int x) {
+            return fibonacci[x];
+        }
+
+        private long[] fibonacci;
+        private final Greedy<Long, ArrayList<Long>> greedy;
+    }
+
     /**
      * Test method for Zeckendorf
      */
     @Test
     public void testZeckendorf() {
-        Zeckendorf z = new Zeckendorf();
-        PrivateMethodTester t = new PrivateMethodTester(z);
+        GreedyZ target = new GreedyZ();
+        PrivateMethodTester t = new PrivateMethodTester(target);
         assertEquals(2, t.invokePrivate("size"));
         assertEquals(1L, t.invokePrivate("fib", 0));
         assertEquals(1L, t.invokePrivate("fib", 1));
@@ -26,8 +107,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testExtendFibonacci() {
-        Zeckendorf z = new Zeckendorf();
-        PrivateMethodTester t = new PrivateMethodTester(z);
+        GreedyZ target = new GreedyZ();
+        PrivateMethodTester t = new PrivateMethodTester(target);
         t.invokePrivate("extendFibonacci");
         assertEquals(4, t.invokePrivate("size"));
         assertEquals(2L, t.invokePrivate("fib", 2));
@@ -46,8 +127,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testCalculateFibonacci() {
-        Zeckendorf z = new Zeckendorf();
-        PrivateMethodTester t = new PrivateMethodTester(z);
+        GreedyZ target = new GreedyZ();
+        PrivateMethodTester t = new PrivateMethodTester(target);
         t.invokePrivate("ensureFibonacci", 5L);
         assertEquals(2L, t.invokePrivate("calculateFibonacci", 2));
         assertEquals(3L, t.invokePrivate("calculateFibonacci", 3));
@@ -60,8 +141,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testEnsureFibonacci() {
-        Zeckendorf z = new Zeckendorf();
-        PrivateMethodTester t = new PrivateMethodTester(z);
+        GreedyZ target = new GreedyZ();
+        PrivateMethodTester t = new PrivateMethodTester(target);
         t.invokePrivate("ensureFibonacci", 1L);
         assertEquals(2, t.invokePrivate("size"));
         t.invokePrivate("ensureFibonacci", 2L);
@@ -79,8 +160,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testGetLargestFibonacci() {
-        Zeckendorf z = new Zeckendorf();
-        PrivateMethodTester t = new PrivateMethodTester(z);
+        GreedyZ target = new GreedyZ();
+        PrivateMethodTester t = new PrivateMethodTester(target);
         assertEquals(1L, t.invokePrivate("getLargestFibonacci", 1L));
         t.invokePrivate("ensureFibonacci", 2L);
         assertEquals(2L, t.invokePrivate("getLargestFibonacci", 2L));
@@ -94,8 +175,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testGet1() {
-        Zeckendorf z = new Zeckendorf();
-        Iterator<Long> fibs = z.get(10).iterator();
+        GreedyZ target = new GreedyZ();
+        Iterator<Long> fibs = target.run(10L).iterator();
         assertEquals(new Long(8), fibs.next());
         assertEquals(new Long(2), fibs.next());
         assertFalse(fibs.hasNext());
@@ -106,8 +187,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testGet2() {
-        Zeckendorf z = new Zeckendorf();
-        Iterator<Long> fibs = z.get(100).iterator();
+        GreedyZ target = new GreedyZ();
+        Iterator<Long> fibs = target.run(100L).iterator();
         assertEquals(new Long(89), fibs.next());
         assertEquals(new Long(8), fibs.next());
         assertEquals(new Long(3), fibs.next());
@@ -119,8 +200,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testGet3() {
-        Zeckendorf z = new Zeckendorf();
-        Iterator<Long> fibs = z.get(1000).iterator();
+        GreedyZ target = new GreedyZ();
+        Iterator<Long> fibs = target.run(1000L).iterator();
         assertEquals(new Long(987), fibs.next());
         assertEquals(new Long(13), fibs.next());
         assertFalse(fibs.hasNext());
@@ -131,8 +212,8 @@ public class ZeckendorfTest {
      */
     @Test
     public void testGet4() {
-        Zeckendorf z = new Zeckendorf();
-        Iterator<Long> fibs = z.get(10000).iterator();
+        GreedyZ target = new GreedyZ();
+        Iterator<Long> fibs = target.run(10000L).iterator();
         assertEquals(new Long(6765), fibs.next());
         assertEquals(new Long(2584), fibs.next());
         assertEquals(new Long(610), fibs.next());
