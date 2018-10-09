039cd59cf743745aea81b004becce2692dc13565
rhillyard <scalaprof@gmail.com>
2018-10-09 17:27:48 -0400

    Clarified behavior of H class somewhat. Improved API.
    (cherry picked from commit 6920304)

--- src/main/java/edu/neu/coe/info6205/sort/simple/ShellSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/simple/ShellSort.java
@@ -2,6 +2,11 @@ package edu.neu.coe.info6205.sort.simple;
 
 import java.util.*;
 
+/**
+ * Class to implement Shell Sort.
+ *
+ * @param <X> the type of element on which we will be sorting (must implement Comparable).
+ */
 public class ShellSort<X extends Comparable<X>> implements Sort<X> {
 
     /**
@@ -11,6 +16,7 @@ public class ShellSort<X extends Comparable<X>> implements Sort<X> {
      *          1: ordinary insertion sort;
      *          2: use powers of two less one;
      *          3: use the sequence based on 3 (the one in the book): 1, 4, 13, etc.
+     *          4: Sedgewick's sequence (not implemented).
      */
     public ShellSort(int m) {
         this.m = m;
@@ -27,13 +33,16 @@ public class ShellSort<X extends Comparable<X>> implements Sort<X> {
     public void sort(X[] xs, int from, int to) {
         int N = to - from;
         H hh = new H(N);
-        int h = hh.next();
+        int h = hh.first();
         while (h > 0) {
             hSort(h, xs, from, to);
             h = hh.next();
         }
     }
 
+    /**
+     * Private method to h-sort an array.
+     */
     private void hSort(int h, X[] xs, int from, int to) {
         for (int i = h + from; i < to; i++)
             for (int j = i; j >= h + from && Helper.less(xs[j], xs[j - h]); j -= h) {
@@ -43,6 +52,9 @@ public class ShellSort<X extends Comparable<X>> implements Sort<X> {
 
     private final int m;
 
+    /**
+     * Private inner class to provide h (gap) values.
+     */
     private class H {
         @SuppressWarnings("CanBeFinal")
         private int h = 1;
@@ -64,10 +76,29 @@ public class ShellSort<X extends Comparable<X>> implements Sort<X> {
             }
         }
 
-        public int next() {
+        /**
+         * Method to yield the first h value.
+         * NOTE: this may only be called once.
+         *
+         * @return the first (largest) value of h, given the size of the problem (N)
+         */
+        int first() {
+            if (started) throw new RuntimeException("cannot call first more than once");
+            started = true;
+            return h;
+        }
+
+        /**
+         * Method to yield the next h value in the "gap" series.
+         * NOTE: first must be called before next.
+         *
+         * @return the next value of h in the gap series.
+         */
+        int next() {
             if (started) {
                 switch (m) {
                     case 1:
+                        // TODO
                         return 0;
                     case 2:
                         h = (h + 1) / 2 - 1;
@@ -79,12 +110,15 @@ public class ShellSort<X extends Comparable<X>> implements Sort<X> {
                         throw new RuntimeException("invalid m value: " + m);
                 }
             } else {
-                started = true;
-                return h;
+                throw new RuntimeException("cannot call next until first is called");
             }
         }
     }
 
+    /**
+     * An example main program.
+     * @param args the command-line args (ignored).
+     */
     public static void main(String[] args) {
         ShellSort<Integer> s = new ShellSort<>(2);
         Integer[] array = {5, 3, 0, 2, 4, 1, 0, 5, 2, 3, 1, 4};
