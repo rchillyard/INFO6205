4faf420c284cbcce633b436d04174afeb2978112
Robin Hillyard <scalaprof@gmail.com>
2018-10-23 22:30:46 -0400

    Added 3-way quick sort

---
+++ src/main/java/edu/neu/coe/info6205/sort/simple/QuickSort_3way.java
@@ -0,0 +1,47 @@
+package edu.neu.coe.info6205.sort.simple;
+
+public class QuickSort_3way<X extends Comparable<X>> implements Sort<X> {
+    class Partition {
+        final int lt;
+        final int gt;
+
+        public Partition(int lt, int gt) {
+            this.lt = lt;
+            this.gt = gt;
+        }
+    }
+
+    @Override
+    public void sort(X[] a, int from, int to) {
+        int lo = from;
+        int hi = to - 1;
+        if (hi <= lo) return;
+        Partition partition = partition(a, lo, hi);
+        sort(a, lo, partition.lt - 1);
+        sort(a, partition.gt + 1, hi);
+    }
+
+    public Partition partition(X[] a, int lo, int hi) {
+        int lt = lo, gt = hi;
+        if (a[lo].compareTo(a[hi])>0) swap(a, lo,hi);
+        X v = a[lo];
+        int i = lo + 1;
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) swap(a, lt++, i++);
+            else if (cmp > 0) swap(a, i, gt--);
+            else i++;
+        }
+        return new Partition(lt, gt);
+    }
+
+    // exchange a[i] and a[j]
+    public static void swap(Object[] a, int i, int j) {
+        Object temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+
+}
+
