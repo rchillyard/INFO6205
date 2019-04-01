26e1ebafca74ad35c41808d01bcef6151b0ba00c
Robin Hillyard <scalaprof@gmail.com>
2019-04-01 13:36:58 -0400

    Added code for Quiz6 comparisons
    Also updated .gitignore

--- src/test/java/edu/neu/coe/info6205/sort/simple/InsertionSortTest.java
+++ src/test/java/edu/neu/coe/info6205/sort/simple/InsertionSortTest.java
@@ -30,4 +30,36 @@ public class InsertionSortTest {
         System.out.println(sorter.toString());
     }
 
+    @Test
+    public void sortQuiz6() throws Exception {
+        assertEquals("epTy",testWord("Type"));
+        assertEquals("aABb",testWord("BabA"));
+        assertEquals("Be?y",testWord("By?e"));
+    }
+
+    private String testWord(String word) {
+        final char[] charsX = word.toCharArray();
+        final Character[] chars = new Character[charsX.length];
+        for (int i=0; i<chars.length; i++) chars[i] = charsX[i];
+        Helper<Character> helper = new Quiz6Helper("InsertionSort", chars.length);
+        InsertionSort<Character> sorter = new InsertionSort<>(helper);
+        Character[] sorted = sorter.sort(chars);
+        final StringBuilder result = new StringBuilder();
+        for (Character x: sorted) result.append(x);
+        return result.toString();
+    }
+
+    class Quiz6Helper extends Helper<Character> {
+
+        public Quiz6Helper(String description, int n) {
+            super(description, n);
+        }
+
+        @Override
+        boolean less(Character v, Character w) {
+            if (Character.isAlphabetic(v) && Character.isAlphabetic(w))
+                return super.less(Character.toLowerCase(v), Character.toLowerCase(w));
+            else return false;
+        }
+    }
 }
