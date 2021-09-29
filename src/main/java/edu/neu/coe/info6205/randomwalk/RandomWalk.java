c008290c6b602c42f05ef8d26132a6163edccf10
Zhijie <l445476530@qq.com>
2021-09-29 11:57:25 -0400

    Add Assign1

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -21,6 +21,8 @@ public class RandomWalk {
      */
     private void move(int dx, int dy) {
         // TO BE IMPLEMENTED
+        x += dx;
+        y += dy;
     }
 
     /**
@@ -30,6 +32,9 @@ public class RandomWalk {
      */
     private void randomWalk(int m) {
         // TO BE IMPLEMENTED
+        for(int i=0; i<m; i++) {
+            randomMove();
+        }
     }
 
     /**
@@ -49,7 +54,7 @@ public class RandomWalk {
      */
     public double distance() {
         // TO BE IMPLEMENTED
-        return 0;
+        return Math.sqrt(x*x + y*y);
     }
 
     /**
