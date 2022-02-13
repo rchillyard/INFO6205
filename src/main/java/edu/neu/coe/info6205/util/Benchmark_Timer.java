520035576f931cd40db90cd40438fd46aed73a87
Naina Rajan <rajan.nai@northeastern.edu>
2022-02-12 19:33:50 -0500

    Benchmark - Assignment2

--- src/main/java/edu/neu/coe/info6205/util/Benchmark_Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Benchmark_Timer.java
@@ -4,11 +4,13 @@
 
 package edu.neu.coe.info6205.util;
 
+import edu.neu.coe.info6205.sort.elementary.InsertionSort;
+
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
-
+import java.util.Random;
 import static edu.neu.coe.info6205.util.Utilities.formatWhole;
 
 /**
@@ -50,7 +52,7 @@ public class Benchmark_Timer<T> implements Benchmark<T> {
      */
     @Override
     public double runFromSupplier(Supplier<T> supplier, int m) {
-        logger.info("Begin run: " + description + " with " + formatWhole(m) + " runs");
+        logger.info( description + " with " + formatWhole(m) + " runs");
         // Warmup phase
         final Function<T, T> function = t -> {
             fRun.accept(t);
@@ -118,11 +120,75 @@ public class Benchmark_Timer<T> implements Benchmark<T> {
     public Benchmark_Timer(String description, Consumer<T> f) {
         this(description, null, f, null);
     }
-
+    public static final int N = 4000;
     private final String description;
     private final UnaryOperator<T> fPre;
     private final Consumer<T> fRun;
     private final Consumer<T> fPost;
 
     final static LazyLogger logger = new LazyLogger(Benchmark_Timer.class);
+
+    public static void main(String[] args) {
+        int m = 20;
+        int n = 2000;
+        Random rand = new Random();
+
+        for (int l = 0; l < 5; l++) {
+            int N = n;
+            InsertionSort<Integer> objInsSort = new InsertionSort<>();
+            Consumer<Integer[]> consumer = array -> objInsSort.sort(array, 0, array.length);
+            Benchmark_Timer<Integer[]> objBenchmarkTimer = new Benchmark_Timer<>("Benchmarking insertion sort: number of integers = " + n, consumer);
+
+            // Get random ordered Integer array
+            Supplier<Integer[]> randomIntegers = () -> {
+                Integer[] arr = new Integer[N];
+                for(int i = 0; i < N; i++){
+                    arr[i] = rand.nextInt();
+                }
+                return arr;
+            };
+
+            // Get Partially ordered Integer array : first half sorted, second half random array
+            Supplier<Integer[]> partiallyOrdered = () -> {
+                Integer[] arr = new Integer[N];
+                for(int i = 0; i < N/2; i++){
+                    arr[i] = i;
+                }
+                for(int i = N/2; i < N; i++){
+                    arr[i] = rand.nextInt();
+                }
+                return arr;
+            };
+
+            // Get sorted integer array
+            Supplier<Integer[]> orderedIntegers = () -> {
+                Integer[] arr = new Integer[N];
+                for(int i = 0; i < N; i++){
+                    arr[i] = i;
+                }
+                return arr;
+            };
+
+            // Get reversed ordered integer array
+            Supplier<Integer[]> reversedOrdered = () -> {
+                Integer[] arr = new Integer[N];
+                for(int i = 0; i < N; i++){
+                    arr[i] = N-i;
+                }
+                return arr;
+            };
+
+            double orderedTime = objBenchmarkTimer.runFromSupplier(orderedIntegers, m);
+            double randomTime = objBenchmarkTimer.runFromSupplier(randomIntegers, m);
+            double partialTime = objBenchmarkTimer.runFromSupplier(partiallyOrdered, m);
+            double reversedTime = objBenchmarkTimer.runFromSupplier(reversedOrdered, m);
+
+            System.out.println("No of integers" + n);
+            System.out.println("Average time of sorted array is " + orderedTime + " milli seconds");
+            System.out.println("Average time of random array is " + randomTime + " milli seconds");
+            System.out.println("Average time of partially ordered array is " + partialTime + " milli seconds");
+            System.out.println("Average time of reversed ordered array is " + reversedTime + " milli seconds");
+            n = n * 3;
+        }
+    }
 }
