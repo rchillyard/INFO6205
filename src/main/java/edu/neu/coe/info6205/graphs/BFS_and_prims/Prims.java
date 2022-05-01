9d5a241fcdfbf7bbc0c76c0ca7f0ee6619f64c5d
UrviAryamane19 <77515953+UrviAryamane19@users.noreply.github.com>
2022-05-01 11:55:04 -0400

    Update Prims.java

--- src/main/java/edu/neu/coe/info6205/graphs/BFS_and_prims/Prims.java
+++ src/main/java/edu/neu/coe/info6205/graphs/BFS_and_prims/Prims.java
@@ -1,3 +1,11 @@
+/*  
+*  For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+*  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+*
+*  @author Robert Sedgewick
+*  @author Kevin Wayne 
+*/
+
 package edu.neu.coe.info6205.graphs.BFS_and_prims;
 
 import java.util.LinkedList;
