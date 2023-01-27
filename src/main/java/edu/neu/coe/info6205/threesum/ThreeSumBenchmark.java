797e96511c164ca74af9ccaf815a590b04b83274
Mehul-Natu <natu.m@northeastern.edu>
2023-01-27 17:16:01 -0500

    Added quadratic with calipers in benchmark

--- src/main/java/edu/neu/coe/info6205/threesum/ThreeSumBenchmark.java
+++ src/main/java/edu/neu/coe/info6205/threesum/ThreeSumBenchmark.java
@@ -20,6 +20,7 @@ public class ThreeSumBenchmark {
     public void runBenchmarks() {
         System.out.println("ThreeSumBenchmark: N=" + n);
         benchmarkThreeSum("ThreeSumQuadratic", (xs) -> new ThreeSumQuadratic(xs).getTriples(), n, timeLoggersQuadratic);
+        benchmarkThreeSum("ThreeSumQuadraticWithCalipers", (xs) -> new ThreeSumQuadraticWithCalipers(xs).getTriples(), n, timeLoggersQuadratic);
         benchmarkThreeSum("ThreeSumQuadrithmic", (xs) -> new ThreeSumQuadrithmic(xs).getTriples(), n, timeLoggersQuadrithmic);
         benchmarkThreeSum("ThreeSumCubic", (xs) -> new ThreeSumCubic(xs).getTriples(), n, timeLoggersCubic);
     }
